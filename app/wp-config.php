cce651fedab3d2cb65759a05dac1c47c6481fddc
Corvin <corvin@corvin-pc.(none)>
2011-12-21 18:43:22 +0200

    update.sh

--- app/wp-config.php
+++ app/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'bumboo_taobao');
+define('DB_NAME', 'taobao');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'bumboo_taobao');
+define('DB_USER', 'root');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'Aa111222333');
+define('DB_PASSWORD', 'root');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
