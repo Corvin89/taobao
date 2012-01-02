798686a6712a58da69022673ec66a09cc32e8cb2
Korkishko Sergey <Spiritvoin88@gmail.com>
2012-01-02 14:26:13 +0200

    debug #8

--- app/wp-content/themes/taobao/js/timer.js
+++ app/wp-content/themes/taobao/js/timer.js
@@ -1,17 +1,36 @@
-var interval = setInterval("getCurrentTime()", 1000);
+var date = new Date();
+var hours = clientHoursMinusServerHours(date);
+var minutes = clientMinutesMinusServerMinutes(date);
+var interval = setInterval("getCurrentTime(date,hours,minutes)", 1000);
 
-function getCurrentTime() {
-    var client = new Date();
-    var clientHours = parseInt((client.getHours()).toString());
-    var clientMinutes = parseInt((client.getMinutes()).toString());
+function clientHoursMinusServerHours(date) {
+    var client = date;
+    var clientHours = client.getHours();
+    clientHours = parseInt(clientHours.toString());
     var differenceHours = serverHours - clientHours;
+    return differenceHours;
+}
+
+function clientMinutesMinusServerMinutes(date) {
+    var client = date;
+    var clientMinutes = client.getMinutes();
+    clientMinutes = parseInt(clientMinutes.toString());
     var differenceMinutes = serverMinutes - clientMinutes;
+    return differenceMinutes;
+}
+
+function getCurrentTime(date,differenceHours, differenceMinutes) {
+    var client = new Date();
+    var clientHours = client.getHours();
+    var clientMinutes = client.getMinutes();
+    var clientHours = parseInt(clientHours.toString());
+    var clientMinutes = parseInt(clientMinutes.toString());
     var newClientHours = clientHours + differenceHours;
     var newClientMinutes = clientMinutes + differenceMinutes;
     timer(newClientHours, newClientMinutes, 10, 19);
 }
 
-function addZero(i) {
+function appZero(i) {
     if (i < 10) {
         i = "0" + i;
     }
@@ -24,22 +43,22 @@ function timer(newClientHours, newClientMinutes, startOfWork, endOfWork) {
     } else {
         timeLeft(newClientHours, newClientMinutes, startOfWork);
     }
-    function timeLeft(newClientHours, newClientMinutes, HoursTo) {
-        if (newClientHours < HoursTo) {
-            var hoursLeft =  HoursTo - newClientHours -1;
+    function timeLeft(newClientHours, newClientMinutes, hoursTo) {
+        if (newClientHours < hoursTo) {
+            var hoursLeft = hoursTo - newClientHours - 1;
             var minutesLeft = 59 - newClientMinutes;
-            var str ='До начала рабочего дня осталось: ';
-            var clas ='work';
+            var str = 'До конца рабочего дня осталоь: ';
+            var cssClass = 'work';
         } else {
-            var hoursLeft = 23 - newClientHours + HoursTo;
+            var hoursLeft = 23 - newClientHours + hoursTo;
             var minutesLeft = 59 - newClientMinutes;
-            var str ='До конца рабочего дня осталоь: ' ;
-            var clas ='suspend';
+            var str = 'До начала рабочего дня осталоь: ';
+            var cssClass = 'suspend';
         }
-        minutesLeft = addZero(minutesLeft);
-        hoursLeft = addZero(hoursLeft);
+        minutesLeft = appZero(minutesLeft);
+        hoursLeft = appZero(hoursLeft);
         var hours = hoursLeft + ":" + minutesLeft;
         $("#alarm").html(str + hours);
-        $("#alarm").addClass('suspend');
+        $("#alarm").attr('class', cssClass);
     }
 }
