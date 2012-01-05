8f2cdf3c46c8ddacafabceea922959d49b7c5ba8
Korkishko Sergey <Spiritvoin88@gmail.com>
2012-01-05 16:33:54 +0200

    окна срочных сообщений  creating messages slide

--- app/wp-content/themes/taobao2/functions.php
+++ app/wp-content/themes/taobao2/functions.php
@@ -8,4 +8,42 @@ register_nav_menus(array(
     'top' => 'Верхнее меню',
 
 ));
-?>
+
+
+
+add_action('init', 'messages_slider');
+
+function messages_slider()
+{
+
+    $eventlabels = array(
+        'name' => 'Clients',
+        'singular_name' => 'Clients',
+        'add_new' => 'Add Client',
+        'add_new_item' => 'Add Client',
+        'edit_item' => 'Add New Client',
+        'new_item' => 'New Client',
+        'view_item' => 'View Client',
+        'search_items' => 'Search Clients',
+        'not_found' =>  'No Clients Found',
+        'not_found_in_trash' => 'No Clients Found in Trash',
+        'parent_item_colon' => '',
+        'menu_name' => 'Clients'
+
+    );
+    $eventargs = array(
+        'labels' => $eventlabels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array('title', 'editor'));
+
+    register_post_type('messages_slider',$eventargs);
+}
