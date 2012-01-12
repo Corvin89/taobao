109f79b8efb2848709973ceea225f48001639f87
Corvin <corvin@corvin-pc.(none)>
2012-01-12 17:18:20 +0200

    Имплементация всех страниц меню

--- app/wp-content/themes/taobao2/page-blog.php
+++ app/wp-content/themes/taobao2/page-blog.php
@@ -1,134 +1,208 @@
 <?php get_header(); ?>
 
 <section id="container">
-    <section id="content">
-        <h2 class="title">Блог Taobao.ru.com</h2>
-        <span class="post">Всего записей: 137</span>
-        <div class="top"></div>
-        <div class="body">
-            <div class="page">
-                <h2>Какие цвета актуальны в этом сезоне?</h2>
-                <p>Казалось бы, осень – скукота и серость… Но это не для нас, если знать, какую цветовую гамму нужно предпочесть в этом сезоне!</p>
-                <table cellpadding="0" cellspacing="0">
-                    <tr>
-                        <td><img src="<?php bloginfo('template_directory'); ?>/img/img.gif" class="alignleft" alt="" title="" /></td>
-                        <td>
-                            <p><b>Непревзойденный тренд сезона – синий,</b> а также все его многообразные оттенки.</p>
-                            <p>Однако не переусердствуйте! Помните, этот цвет идеально подходит для смелых, сексуальных девушек, роковых дам. Можно успешно сочетать красный и чёрный.</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><img src="<?php bloginfo('template_directory'); ?>/img/img.gif" class="alignleft" alt="" title="" /></td>
-                        <td>
-                            <p><b>Непревзойденный тренд сезона – синий,</b> а также все его многообразные оттенки.</p>
-                            <p>Однако не переусердствуйте! Помните, этот цвет идеально подходит для смелых, сексуальных девушек, роковых дам. Можно успешно сочетать красный и чёрный.</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><img src="<?php bloginfo('template_directory'); ?>/img/img.gif" class="alignleft" alt="" title="" /></td>
-                        <td>
-                            <p><b>Непревзойденный тренд сезона – синий,</b> а также все его многообразные оттенки.</p>
-                            <p>Однако не переусердствуйте! Помните, этот цвет идеально подходит для смелых, сексуальных девушек, роковых дам. Можно успешно сочетать красный и чёрный.</p>
-                        </td>
-                    </tr>
-                </table>
-                <p>Предстоящий сезон скучным не будет однозначно! Следуйте нашим советам, а taobao.ru.com поможет Вам справиться с осенней хандрой! модные цвета сезона.</p>
-                <div class="article">
-                    <span class="label">корсеты, перчатки, платья, стиль Gothic & Lolita, туфли на платформе, украшения для волос, чулки, юбки</span>
-                    <a class="com">15</a>
-                    <span class="data">02.11.2011</span>
+<section id="content">
+    <h2 class="title">Блог Taobao.ru.com</h2>
+    <span class="post">Всего записей: 137</span>
+    <div class="top"></div>
+    <div class="body">
+        <div class="page">
+            <h2><a href="#">В долине кукол</a></h2>
+            <p>Все мы, девушки, были совсем недавно маленькими девочками. В куколки играли. Теперь немного подросли. В куклы играть уже несолидно. А так хочется, чтобы хоть что-нибудь напоминало о куклах. <a href="#">Есть решение!</a></p>
+            <div class="article">
+                <span class="label">корсеты, перчатки, платья, стиль Gothic & Lolita, туфли на платформе, украшения для волос, чулки, юбки</span>
+                <a class="com">15</a>
+                <span class="data">02.11.2011</span>
+            </div>
+            <div class="all">
+                <div class="left-box">
+                    <h2 class="post">Новые статьи</h2>
+                    <ul>
+                        <li>
+                            <p><a href="#">В долине кукол</a></p>
+                            <p><span>04.11.2011  15 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Пуловер. Достоин мужского внимания</a></p>
+                            <p><span>03.11.2011   7 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Информация для покупателей: вес вещей</a></p>
+                            <p><span>02.11.2011    15 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">В чём встречать Новый 2012 год?</a></p>
+                            <p><span>01.11.2011    29 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Какие цвета актуальны в этом сезоне?</a></p>
+                            <p><span>29.10.2011   3 комментария</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Какие цвета актуальны в этом сезоне?</a></p>
+                            <p><span>29.10.2011   3 комментария</span></p>
+                        </li>
+                    </ul>
                 </div>
-                <div class="soc"><img src="<?php bloginfo('template_directory'); ?>/img/soc.gif" alt="" title="" /></div>
-                <div class="comentars">
-                    <h3>Комментарии</h3>
-                    <form action="" method="post">
-                        <div class="item">
-                            <div class="ava"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ava.gif" alt="" title="" /></a></div>
-                            <textarea></textarea>
-                        </div>
-                        <div class="coment">
-                            <div class="ava"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ava.gif" alt="" title="" /></a></div>
-                            <div class="boxer">
-                                <p><a href="#" class="name">T-ula</a> <span class="date">22 ноября 2011, 20:55</span></p>
-                                <p>Здравствуйте! Хотела бы заказать 5 брендовых курточек (одной модели), не будет ли проблем с таможней? Обычно многие поставщики не советуют заказывать более 4-х шт.  одной модели?</p>
-                            </div>
-                            <div class="podcoment">
-                                <div class="ava"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ava.gif" alt="" title="" /></a></div>
-                                <div class="boxe">
-                                    <p><a href="#" class="nameblue">T-ula</a> <span class="date">22 ноября 2011, 20:55</span></p>
-                                    <p>Здравствуйте! Хотела бы заказать 5 брендовых курточек (одной модели), не будет ли проблем с таможней? Обычно многие поставщики не советуют заказывать более 4-х шт.  одной модели?</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="coment">
-                            <div class="ava"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/ava.gif" alt="" title="" /></a></div>
-                            <div class="boxer">
-                                <p><a href="#" class="name">T-ula</a> <span class="date">22 ноября 2011, 20:55</span></p>
-                                <p>Здравствуйте! Хотела бы заказать 5 брендовых курточек (одной модели), не будет ли проблем с таможней? Обычно многие поставщики не советуют заказывать более 4-х шт.  одной модели?</p>
-                            </div>
-                        </div>
-                    </form>
-                    <ul class="rss">
-                        <li><a href="#" class="rss">RSS</a></li>
-                        <li><a href="#" class="mail">Подписаться по e-mail</a></li>
+                <div class="left-box right-box">
+                    <h2 class="read">Полезно почитать</h2>
+                    <ul>
+                        <li>
+                            <p><a href="#">Первые шаги на Таобао. Что такое Таобао? Справочная информация о компании, предлагаемых услугах.</a></p>
+                            <p><span>04.11.2011  15 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Первые шаги на таобао. Как искать вещи?</a></p>
+                            <p><span>04.11.2011  15 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Как правильно выбирать продавца? Все доступно и с картинками.</a></p>
+                            <p><span>04.11.2011  15 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Как правильно выбирать продавца? Все доступно и с картинками.</a></p>
+                            <p><span>04.11.2011  15 комментариев</span></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Как правильно выбирать продавца? Все доступно и с картинками.</a></p>
+                            <p><span>04.11.2011  15 комментариев</span></p>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </div>
-        <div class="bottom"></div>
-    </section>
-    <div class="right">
-        <div class="boxen">
-            <h2>Внутренний курс <span>Taobao.ru.com:</span></h2>
-            <span class="calcul">5,3</span>
-            <div class="calcul">
-                <a href="#">Калькулятор</a>
-                <p>Рассчитать стоимость товаров с учетом доставки.</p>
+            <div class="all">
+                <div class="line-title">
+                    <h2 class="tegi">Поиск статей по тегам</h2> <span><a href="#" class="rig">все статьи</a> →</span>
+                </div>
+                <div class="tegs">
+                    <img src="<?php bloginfo('template_directory'); ?>/<?php bloginfo('template_directory'); ?>/img/tegs.png" alt="" title="" />
+                </div>
             </div>
-            <div class="top-s"></div>
-            <div class="body-s">
-                <h2>Русский поиск <span>на Taobao.com:</span></h2>
-                <form action="" method="post">
-                    <div class="item">
-                        <label>Введите слово или фразу на <br/> русском языке и нажмите <br/> кнопку “Перевести” </label>
-                        <input type="text" class="text" />
-                        <input type="submit" class="sub" value="Перевести" />
-                    </div>
-                    <div class="item">
-                        <label>Затем нажмите "Поиск на <br/> Taobao" и у вас откроется <br/> страница с результатами поиска.</label>
-                        <input type="text" class="text" />
-                        <input type="submit" class="sub" value="Поиск на Taobao.com" />
-                    </div>
-                    <div class="item">
-                        <a href="#">Видеоинструкция</a>
-                    </div>
-                </form>
+            <div class="all">
+                <h2 class="video">Это интересно</h2>
+                <ul class="video">
+                    <li>
+                        <div class="foto"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/video.gif" alt="" title="" /></a></div>
+                        <div class="text-video">
+                            <p><a href="#">Регистрация на сайте Taobao.ru.com</a></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="foto"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/video.gif" alt="" title="" /></a></div>
+                        <div class="text-video">
+                            <p><a href="#">Регистрация на сайте Taobao.ru.com</a></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="foto"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/video.gif" alt="" title="" /></a></div>
+                        <div class="text-video">
+                            <p><a href="#">Регистрация на сайте Taobao.ru.com</a></p>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="bottom-s"></div>
-            <div class="blog-gree">
-                <div class="blog">
-                    <h2>Новое в блоге</h2>
+            <div class="all">
+                <div class="line-title">
+                    <h2 class="list">Видеоинструкции</h2> <span><a href="#" class="rig">все видео</a> →</span>
+                </div>
+                <div class="left-box">
                     <ul>
                         <li>
-                            <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
-                            <p><b>В чём встречать Новый 2012 год?</b></p>
-                            <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                            <p><a href="#">В долине кукол</a></p>
                         </li>
                         <li>
-                            <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
-                            <p><b>В чём встречать Новый 2012 год?</b></p>
-                            <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                            <p><a href="#">Пуловер. Достоин мужского внимания</a></p>
                         </li>
                         <li>
-                            <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
-                            <p><b>В чём встречать Новый 2012 год?</b></p>
-                            <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                            <p><a href="#">Информация для покупателей: вес вещей</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">В чём встречать Новый 2012 год?</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Какие цвета актуальны в этом сезоне?</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Какие цвета актуальны в этом сезоне?</a></p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="left-box right-box">
+                    <ul>
+                        <li>
+                            <p><a href="#">Первые шаги на Таобао. Что такое Таобао? Справочная информация о компании, предлагаемых услугах.</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Первые шаги на таобао. Как искать вещи?</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Как правильно выбирать продавца? Все доступно и с картинками.</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Как правильно выбирать продавца? Все доступно и с картинками.</a></p>
+                        </li>
+                        <li>
+                            <p><a href="#">Как правильно выбирать продавца? Все доступно и с картинками.</a></p>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
+    <div class="bottom"></div>
+</section>
+<div class="right">
+    <div class="boxen">
+        <h2>Внутренний курс <span>Taobao.ru.com:</span></h2>
+        <span class="calcul">5,3</span>
+        <div class="calcul">
+            <a href="#">Калькулятор</a>
+            <p>Рассчитать стоимость товаров с учетом доставки.</p>
+        </div>
+        <div class="top-s"></div>
+        <div class="body-s">
+            <h2>Русский поиск <span>на Taobao.com:</span></h2>
+            <form action="" method="post">
+                <div class="item">
+                    <label>Введите слово или фразу на <br/> русском языке и нажмите <br/> кнопку “Перевести” </label>
+                    <input type="text" class="text" />
+                    <input type="submit" class="sub" value="Перевести" />
+                </div>
+                <div class="item">
+                    <label>Затем нажмите "Поиск на <br/> Taobao" и у вас откроется <br/> страница с результатами поиска.</label>
+                    <input type="text" class="text" />
+                    <input type="submit" class="sub" value="Поиск на Taobao.com" />
+                </div>
+                <div class="item">
+                    <a href="#">Видеоинструкция</a>
+                </div>
+            </form>
+        </div>
+        <div class="bottom-s"></div>
+        <div class="blog-gree">
+            <div class="blog">
+                <h2>Новое в блоге</h2>
+                <ul>
+                    <li>
+                        <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
+                        <p><b>В чём встречать Новый 2012 год?</b></p>
+                        <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                    </li>
+                    <li>
+                        <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
+                        <p><b>В чём встречать Новый 2012 год?</b></p>
+                        <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                    </li>
+                    <li>
+                        <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
+                        <p><b>В чём встречать Новый 2012 год?</b></p>
+                        <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 </section>
 </div>
 
---
+++ app/wp-content/themes/taobao2/page-preimushhestva.php
@@ -0,0 +1,114 @@
+<?php get_header(); ?>
+
+<section id="container">
+    <section id="content">
+        <h2 class="title">Блог Taobao.ru.com</h2>
+        <span class="post">Всего записей: 137</span>
+        <div class="top"></div>
+        <div class="body">
+            <div class="allbox padding">
+                <div class="work">
+                    <h3>Мы являемся посредником taobao.com и других аукционов Китая:</h3>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon.gif" alt="" title="" /></a></div>
+                        <p>У нас работают русскоговорящие менеджеры, владеющие не только русским, но также китайским и английским языками. Работаем вместе и всегда рады обслужить Вас!</p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon1.gif" alt="" title="" /></a></div>
+                        <p>Главный офис компании находится всего в километре от Китая: в г. Благовещенске, Амурской области. В Китае, в г.Хэйхэ расположены дополнительный офис и склад компании. Пересекаем границу всего за 10 минут! Именно благодаря нашему месторасположению, Вы можете быть уверены, что Ваш заказ проверится до того, как пересечет границу.</p>
+                    </div>
+                </div>
+                <div class="work">
+                    <h3>У вас есть товар, хотите недорого перевезти его в Россию? Тогда вам – к нам!</h3>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon2.gif" alt="" title="" /></a></div>
+                        <p>У нас отсутствуют дополнительные таможенные пошлины, задержки, ограничения по цене и весу, которые могут возникнуть при отправке груза через EMS и ChinaPost. У нас перевозка Карго!</p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon3.gif" alt="" title="" /></a></div>
+                        <p>Работая с клиентами, прежде всего, мы стремимся зарекомендовать себя как самого надежного посредника <a href="#">taobao.com</a></p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon4.gif" alt="" title="" /></a></div>
+                        <p>Исключительно точные сроки выполнения заказа! Повторимся: находимся на границе с Китаем. Так сказать, одна нога здесь – другая там.</p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon5.gif" alt="" title="" /></a></div>
+                        <p>Принимаем заказы любой сложности, габаритов и широкого ассортимента (кроме наркотиков, оружия и запрещенных для перевозки товаров) – доставим  все и в любых количествах.</p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon6.gif" alt="" title="" /></a></div>
+                        <p>Работаем со всеми аукционами и интернет-магазинами Китая.</p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon7.gif" alt="" title="" /></a></div>
+                        <p>Сроки доставки могут сокращаться за счет установленных особых тесных связей с проверенными поставщиками.</p>
+                    </div>
+                    <div class="desk">
+                        <div class="icon"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon8.gif" alt="" title="" /></a></div>
+                        <p>Предлагаем простые и проверенные <a href="#">способы доставки по России.</a></p>
+                    </div>
+                </div>
+                <div class="banner">
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/taobao.gif" alt="" title="" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="bottom"></div>
+    </section>
+    <div class="right">
+        <div class="boxen">
+            <h2>Внутренний курс <span>Taobao.ru.com:</span></h2>
+            <span class="calcul">5,3</span>
+            <div class="calcul">
+                <a href="#">Калькулятор</a>
+                <p>Рассчитать стоимость товаров с учетом доставки.</p>
+            </div>
+            <div class="top-s"></div>
+            <div class="body-s">
+                <h2>Русский поиск <span>на Taobao.com:</span></h2>
+                <form action="" method="post">
+                    <div class="item">
+                        <label>Введите слово или фразу на <br/> русском языке и нажмите <br/> кнопку “Перевести” </label>
+                        <input type="text" class="text" />
+                        <input type="submit" class="sub" value="Перевести" />
+                    </div>
+                    <div class="item">
+                        <label>Затем нажмите "Поиск на <br/> Taobao" и у вас откроется <br/> страница с результатами поиска.</label>
+                        <input type="text" class="text" />
+                        <input type="submit" class="sub" value="Поиск на Taobao.com" />
+                    </div>
+                    <div class="item">
+                        <a href="#">Видеоинструкция</a>
+                    </div>
+                </form>
+            </div>
+            <div class="bottom-s"></div>
+            <div class="blog-gree">
+                <div class="blog">
+                    <h2>Новое в блоге</h2>
+                    <ul>
+                        <li>
+                            <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
+                            <p><b>В чём встречать Новый 2012 год?</b></p>
+                            <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                        </li>
+                        <li>
+                            <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
+                            <p><b>В чём встречать Новый 2012 год?</b></p>
+                            <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                        </li>
+                        <li>
+                            <p><span class="data">02.11.2011</span> <span class="com">15</span></p>
+                            <p><b>В чём встречать Новый 2012 год?</b></p>
+                            <p>По восточному календарю покровителем наступающего года будет <a href="#">чёрный водяной Дракон.</a></p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+
+<?php get_footer();?>
