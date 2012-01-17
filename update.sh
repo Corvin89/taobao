61b3964547084b2736caf654008657dfef6dcebf
Korkishko Sergey <Spiritvoin88@gmail.com>
2012-01-17 18:40:09 +0200

    исправил карявый update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #git pull origin master
 mysql -uadmin -padmin -e "drop database taobao;"
 mysql -uadmin -padmin -e "create database taobao;"
-mysql -uadmin -padmin taobao < www/database.sql
-mysql -uadmin -padmin -e "UPDATE wp_options SET option_value = 'http://taobao/app/' WHERE option_name = 'home' OR option_name = 'siteurl';" taobao
+mysql -uadmin -padmin taobao < backup/database.sql
+mysql -uadmin -padmin -e "UPDATE wp_options SET option_value = 'http://taobao.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" taobao
 #git stash pop
 
